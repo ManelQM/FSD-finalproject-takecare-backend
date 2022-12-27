7fedefd2484f9dd8910adc99fe83d8656f2efd6e
Manel <quemonmanel@gmail.com>
2022-12-27 23:31:08 +0100

    feat: app.js db + port connections

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const express = require ('express'); 
-const app = express;
-const PORT = process.env.PORT || 3007; 
-const db = require ('./db.js'); 
-const router = require ('router.js'); 
+    
+    
+    const express = require ('express'); 
+    const app = express;
+    const PORT = process.env.PORT || 3000; // Configuramos puerto 
+    const db = require ('./db.js'); 
+    const router = require ('router.js'); 
 
 
+    // Middleware 
+
+    app.use(express.json()); 
+
+    //Routes
+
+    app.get('/',(req,res) => {res.send('Nice to see you :)');});
+    app.use(router); 
+
+    //DB Connection 
+
+    db.then(() => {
+
+        // Starting Server
+
+        app.listen(PORT, () => console.log (`Ciclo de Trantor nº ${PORT}`.bgGreen.black));
+    })
+
+    .catch((error) => console.log (error.message)); 
+    
 
