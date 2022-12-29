6a5cba7c76d59b44b092032eed41a83086d00c38
Manel <quemonmanel@gmail.com>
2022-12-29 22:20:22 +0100

    feat:seeders+router+firstendpoints

--- router.js
+++ router.js
@@ -1,16 +1,22 @@
-const router = require('express').Router();
+    
+    const router = require('express').Router();
+   
 
-// Middlewares
-const auth = require('./middlewares/auth');
+       
 
-//Importamos Routes definidas en views
-// const MovieRouter = require('./views/MovieRouter');
-// const CategoryRouter = require('./views/CategoryRouter');
-// const UserRouter = require('./views/UserRouter');
+    // Middlewares
+    const auth = require('./middlewares/auth');
 
-//Rutas
-// router.use('/api', UserRouter); //Login and register routes
-// router.use('/movies',auth, MovieRouter); //add auth
-// router.use('/categories',auth, CategoryRouter);
+    //Importamos Routes definidas en views
+    // const MovieRouter = require('./views/MovieRouter');
+    // const CategoryRouter = require('./views/CategoryRouter');
+    const UsersRouter = require('./views/UsersRouter');
+       
 
-module.exports = router;
+
+    //Rutas
+    router.use('/users', UsersRouter); //Login and register routes
+    // router.use('/movies',auth, MovieRouter); //add auth
+    // router.use('/categories',auth, CategoryRouter);
+
+    module.exports = router;
