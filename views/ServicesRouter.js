c199cc476ab50170a98fad632949f69ed663fe70
Manel <quemonmanel@gmail.com>
2023-01-02 23:11:21 +0100

    feat services controller

--- views/ServicesRouter.js
+++ views/ServicesRouter.js
@@ -5,3 +5,9 @@
         const {isValidRole,authBearerMiddleware,} = require("../middlewares/AuthMiddleware");
 
         
+        router.get('/services/allmyservices');
+        router.patch('/services/updateservice');
+        router.get('/services/bycontract');
+        router.delete('/services/delete'); 
+
+        module.exports = router; 
