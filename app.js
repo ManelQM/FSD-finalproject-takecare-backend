d5e0d1a441d773d13803742c21b5197381bf3e04
Manel <quemonmanel@gmail.com>
2023-01-02 11:05:11 +0100

    feat:servicescontroller

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
       
-    const express = require ('express'); 
-    const db = require ('./db/db'); 
-    const router = require ('./router.js'); 
-    const app = express();
-    const PORT = process.env.PORT || 3000; // Configuramos puerto 
-    const cors = require("cors");
-  
-
-    // Middleware 
+        const express = require ('express'); 
+        const db = require ('./db/db'); 
+        const router = require ('./router.js'); 
+        const app = express();
+        const PORT = process.env.PORT || 3000; // Configuramos puerto 
+        const cors = require("cors");
     
-    app.use(cors(corsOptions));
-    app.use(express.json()); 
 
-    //Routes
-
-    app.use(router); 
-
-
-    //Config Cors Options
-    var corsOptions = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    };
-
-
-
-    //DB Connection 
-
-    app.listen(PORT, () => {
-        console.log (`Welcome to TakeCare at Port ${PORT }`);
+        // Middleware 
         
-        db.then( () => {
-            console.log ('Nice to see you again (DB connected)');
-        }).catch(error => {
-            console.log ('This thing between us its not working: ' + error)
-        }) 
-    
-    })
-    
+        app.use(cors(corsOptions));
+        app.use(express.json()); 
+
+        //Routes
+
+        app.use(router); 
+
+
+        //Config Cors Options
+        var corsOptions = {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        };
+
+
+
+        //DB Connection 
+
+        app.listen(PORT, () => {
+            console.log (`Welcome to TakeCare at Port ${PORT }`);
+            
+            db.then( () => {
+                console.log ('Nice to see you again (DB connected)');
+            }).catch(error => {
+                console.log ('This thing between us its not working: ' + error)
+            }) 
+        
+        })
+        
 
--- seeders/20221229190327-role.js
+++ seeders/20221229190327-role.js
@@ -1,19 +1,19 @@
-'use strict';
+    'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Roles', [{
-      id_role: 1,
-     rolename: "admin"
-    },
-   {
-     id_role: 2,
-     rolename: "user"
-   }], {});
-  },
+    /** @type {import('sequelize-cli').Migration} */
+    module.exports = {
+      async up (queryInterface, Sequelize) {
+        await queryInterface.bulkInsert('Roles', [{
+          id_role: 1,
+        rolename: "admin"
+        },
+      {
+        id_role: 2,
+        rolename: "user"
+      }], {});
+      },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', null, {});
-  }
-};
+      async down (queryInterface, Sequelize) {
+        await queryInterface.bulkDelete('Users', null, {});
+      }
+    };
