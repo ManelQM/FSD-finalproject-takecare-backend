5b0461882ccb0ca823b40ff3c03b682d915dd0c0
Manel <quemonmanel@gmail.com>
2023-01-02 10:01:23 +0100

    feat:publicationsrouter

--- views/PublicationsRouter.js
+++ views/PublicationsRouter.js
@@ -1,16 +1,22 @@
         
         const express = require('express');
         const router = express.Router();
+        const {isValidRole, isValidUser} = require('../middlewares/authMiddleware');
         const {
             createPublication,
             deletePublication,
+            updateMyPublication,
             getMyPublications,
-            
-        }=
+            getAllPublications,
+            deleteAllMyPublications,
+        } = require('../controllers/PublicationsController'); 
 
-            
-
-
+        router.post("/publications/newpublication",createPublication);
+        router.delete("/publications/delete",deletePublication);
+        router.patch("/publications/updatepublication",updateMyPublication);
+        router.get("/publications/mypublications",getMyPublications);
+        router.get("/publications/all",getAllPublications);
+        router.delete("/publications/deleteallmy",deleteAllMyPublications);
 
 
         module.exports = router;
